2ae8155895a91b6e080457abdf47bf0d96864422
MariaAntonio <syndicato@outlook.com>
2015-12-22 07:56:56 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+#EXTM3U
+#EXTINF:-1, Sport TV 1 HD
+http://maxspeed.from-me.org:8000/live/edgar/edgar1/1205.ts
+
+#EXTINF:-1, Benfica TV
+http://pt-stream.fubotv.com.a.prod.fastly.net/Benfica-TV/ngrp:BenficaHD.stream_all/chunklist_b2628000.m3u8
+
+#EXTINF:-1 tvg-id="0500" group-title="Português" tvg-logo="0500.png", RTP 1
+http://204.107.27.250/live/374.low.stream/playlist.m3u8?id=375f55513f86d64fffb1f92d46bc64fd129c0eafb199637face785329dbb5bfd51b73b42e5f6dd0622d5314df667ec74ca3fbd14c9bcac03ed758a98b22c466d508c4ba02d5ba42edaccd8873c9dff7dbacbbc5f92937ff60bef0aa1e81c5073d568818702ccf5bbcefca60e4a1e116bd1eb41e4ef0013be6de40e794425fd18395828dd232b948e676ce24762ee35246469e3c42e351afb
+
+?>
